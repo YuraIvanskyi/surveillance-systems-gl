4402d86126eddb663bc25ecb7f18846e2192d1a5
Mike <mykhailo.nikolaichuk@gmail.com>
2020-09-08 00:19:39 +0300

    polished aut-cards

--- src/components/aut-card.vue
+++ src/components/aut-card.vue
@@ -3,28 +3,27 @@
     <v-card
       width="320"
       height="380"
-      v-for="project in projects"
-      :key="project.name"
       elevation=12
-      class="px-0 py-0 mx-6 my-0 red lighten-1"
+      class="px-0 py-0 mx-6 my-0 lighten-1"
       :color="project.color"
+      v-ripple="{ class: `${
+        project.color.includes(' ') ?
+        project.color.split(' ')[0] :
+        project.color}--text`}"
+      @click="logSomething(project)"
     >
       <v-container class="py-1" fluid>
         <v-row>
           <v-list-item-title
-            class="headline font-weight-medium  grey lighten-3"
+            :class="getTitleClass(project)"
             align="center"
+            style="height: 32px;"
           >{{project.name}}</v-list-item-title>
         </v-row>
 
         <v-row>
-          <!-- <v-img
-          src="https://diwem.github.io/framework/images/wall.jpg"
-          :lazy-src="`../assets/${project.img}`"
-          height="180
-          "></v-img> -->
-          <!-- <img :src="getCardImage(project)" height="180"/> -->
-          <v-img src="@/assets/a9.jpg" height="180"></v-img>
+          <v-img :src="getCardImage(project)" height="180"/>
+          <!-- <v-img src="@/assets/a9.jpg" height="180"></v-img> -->
         </v-row>
 
         <v-row class="grey lighten-4 pl-2">
@@ -33,8 +32,8 @@
               <span class="font-weight-light">Services on all platforms</span>
               <v-spacer></v-spacer>
               <v-hover disabled>
-                <v-btn text height=24 class="subtitle-1" small>
-                  {{project.servicesAmount}}
+                <v-btn text height=24 class="subtitle-1 no-hover" small>
+                  8
                 </v-btn>
               </v-hover>
             </v-row>
@@ -42,24 +41,24 @@
             <v-row>
               <span class="font-weight-light">Services tests passed</span>
               <v-spacer></v-spacer>
-              <v-btn text height=24 class="subtitle-1" small>
-                {{project.servicesPassed}}
+              <v-btn text height=24 class="subtitle-1 no-hover" small>
+                800
               </v-btn>
             </v-row>
             <v-divider width="275"></v-divider>
             <v-row>
               <span class="font-weight-light">Services tests failed</span>
               <v-spacer></v-spacer>
-              <v-btn text height=24 class="subtitle-1" small>
-                {{project.servicesFailed}}
+              <v-btn text height=24 class="subtitle-1 no-hover" small>
+                555
               </v-btn>
             </v-row>
             <v-divider width="275"></v-divider>
             <v-row>
               <span class="font-weight-light">Services tests not tested</span>
               <v-spacer></v-spacer>
-              <v-btn text height=24 class="subtitle-1" small>
-                {{project.servicesNotTested}}
+              <v-btn text height=24 class="subtitle-1 no-hover" small >
+                3
               </v-btn>
             </v-row>
           </v-container>
@@ -67,10 +66,10 @@
 
         <v-row class="grey lighten-4">
           <span class=" pl-2 py-1 font-weight-light">
-            Last service test: {{project.lastTest}} min ago
+            Last service test: 5 min ago
           </span>
           <v-spacer></v-spacer>
-          <v-btn icon class="mr-2">
+          <v-btn icon class="mr-2 no-hover">
             <v-icon>mdi-information-outline</v-icon>
           </v-btn>
         </v-row>
@@ -82,36 +81,26 @@
 <script>
 export default {
   data() {
-    return {
-      projects: [
-        {
-          name: 'Asphalt 9',
-          img: 'a9.jpg',
-          color: 'red',
-          servicesAmount: 21,
-          servicesPassed: 16,
-          servicesFailed: 3,
-          servicesNotTested: 2,
-          lastTest: 5,
-        },
-        // {
-        //   name: 'My Little Pony',
-        //   img: 'ndot yet',
-        //   color: 'pink',
-        //   servicesAmount: 25,
-        //   servicesPassed: 11,
-        //   servicesFailed: 2,
-        //   servicesNotTested: 1,
-        //   lastTest: 4,
-        // },
-      ],
-    };
+    return {};
   },
   methods: {
     getCardImage(project) {
-      console.log(project);
       return project.img;
     },
+    getTitleClass(project) {
+      console.log(project);
+      return project.name.length <= 15 ? 'headline font-weight-medium  grey lighten-3' : 'subtitle-1 font-weight-black grey lighten-3';
+    },
+    logSomething(project) {
+      console.log(project);
+    },
   },
+  props: ['project'],
 };
 </script>
+
+<style scoped>
+  .no-hover:before {
+    color: transparent
+  }
+</style>
