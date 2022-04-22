cb2540bd93580445b3104a1e68440d8bc1a826d9
Yuriy Ivanskyi <yuraivanskyi@gmail.com>
2022-04-22 19:45:54 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,3 @@
 export default {
-  apiKey: 'AIzaSyC2sHv5z8zp4lqwt-PJtwfOfYVxYNUbKOg',
-  authDomain: 'surveillance-dashboard-593c1.firebaseapp.com',
-  databaseURL: 'https://surveillance-dashboard-593c1.firebaseio.com',
-  projectId: 'surveillance-dashboard-593c1',
-  storageBucket: 'surveillance-dashboard-593c1.appspot.com',
-  messagingSenderId: '699345316472',
-  appId: '1:699345316472:web:8cd05a841bd5ce2e279b8d',
-  measurementId: 'G-BTDHGTDSFL',
+  // not so fast
 };
